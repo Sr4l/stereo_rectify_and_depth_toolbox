cf277d665d809c7ef2d3aa7179de2c43f6dc0b05
Lars Kistner <LarsKistner@sr4l.de>
2026-05-14 16:24:58 +0200

    first qt6 support

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,5 +1,12 @@
-from .param_panel import CameraParamPanel
-from .image_panel import ImagePanel
-from .main_window import StereoCalibrationGUI
+# Qt6 GUI exports
+from .qt_main_window import StereoCalibrationGUI, main
+from .qt_image_panel import ImagePanel, ThumbnailPanel
+from .qt_param_panel import CameraParamPanel
 
-__all__ = ['CameraParamPanel', 'ImagePanel', 'StereoCalibrationGUI']
+__all__ = [
+    "StereoCalibrationGUI",
+    "main",
+    "ImagePanel",
+    "ThumbnailPanel",
+    "CameraParamPanel",
+]
